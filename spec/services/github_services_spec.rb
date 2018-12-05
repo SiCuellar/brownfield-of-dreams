384a76d64372a771343c437d7bb117ba96b74a43
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-12-05 14:13:52 -0700

    added more github service spec expectations

--- spec/services/github_services_spec.rb
+++ spec/services/github_services_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 describe GithubService do
   it " Gets JSON data" do
     VCR.use_cassette("user_repos_casette") do
-
       service = GithubService.new({oath_key: ENV["github_key"]})
-      binding.pry
 
-      
-
+      expect(service.response_repos).to be_a(Array)
+      expect(service.response_repos[0]).to have_key(:name)
+      expect(service.response_repos[0]).to have_key(:owner)
+      expect(service.response_repos[0]).to have_key(:full_name)
     end
-
   end
 end
