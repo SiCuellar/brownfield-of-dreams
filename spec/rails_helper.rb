a798efbfb59b887f3075cc46d1777a04469da9a0
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-12-05 18:05:43 -0700

    added filter for VHS

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,7 @@ VCR.configure do |config|
   config.configure_rspec_metadata!
   config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
   config.allow_http_connections_when_no_cassette = true
+  config.filter_sensitive_data("<GITHUB_API_KEY>") { ENV['github_key']}
 end
 
 
