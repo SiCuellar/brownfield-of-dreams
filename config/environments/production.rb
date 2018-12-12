b8bea5cd9c15f60c229c22807148e0fbccb24347
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-12-12 12:46:29 -0700

    sending account validation email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "personal_project_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
