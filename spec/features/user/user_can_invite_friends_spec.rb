0f9082e4bd0135a0e624ce53546b48211862d5b7
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-12-13 11:38:16 -0700

    Fix test

--- spec/features/user/user_can_invite_friends_spec.rb
+++ spec/features/user/user_can_invite_friends_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 describe 'User ' do
   it 'user can send an invite' do
+
+    expected_username = JSON.parse(File.read("./spec/fixtures/github_user_information.json"))['login']
+
+    stub_request(:any, "https://api.github.com/users/#{expected_username}").
+      with(headers: { 'Authorization' => "token abc"}).
+      to_return(body: File.read("./spec/fixtures/github_user_information.json"))
+
     user_1 = create(:user, token: "abc")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user) {user_1}
@@ -13,12 +20,13 @@ describe 'User ' do
     end
 
     expect(current_path).to eq("/invite")
-    fill_in "github", with: "prestonjarnagin"
+    fill_in "github", with: expected_username
 
     click_on("Send Invite")
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content("Successfully sent invite!")
-
   end
+
+  
 
 end
