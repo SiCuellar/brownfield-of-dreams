794128074a944a300b2249cb5ff500b836f57eb7
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-12-13 11:40:19 -0700

    Write another test

--- spec/features/user/user_can_invite_friends_spec.rb
+++ spec/features/user/user_can_invite_friends_spec.rb
@@ -27,6 +27,32 @@ describe 'User ' do
     expect(page).to have_content("Successfully sent invite!")
   end
 
-  
+  it 'user can not send an invite to a nonexistant username' do
+
+    expected_username = JSON.parse(File.read("./spec/fixtures/github_user_information.json"))['login']
+
+    stub_request(:any, "https://api.github.com/users/#{expected_username}").
+      with(headers: { 'Authorization' => "token abc"}).
+      to_return(body: File.read("./spec/fixtures/github_user_information.json"))
+
+    user_1 = create(:user, token: "abc")
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user) {user_1}
+
+    visit dashboard_path
+
+    within(".email-friend-invite")do
+      click_link("Send an Invite")
+    end
+
+    expect(current_path).to eq("/invite")
+    fill_in "github", with: "someone_else"
+
+    click_on("Send Invite")
+    expect(current_path).to eq(invite_path)
+    expect(page).to have_content("The Github user you selected doesn't have an email address associated with their account.")
+  end
+
+
 
 end
