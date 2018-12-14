da80c5d04d01441f735edd7d7b7c228e408302a6
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-12-13 17:07:03 -0700

    trying to get heroku to run

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "personal_project_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'localhost' }
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.default_url_options = { :host => "https://polar-castle-33210.herokuapp.com/" }
+  config.action_mailer.smtp_settings = { :address => "https://polar-castle-33210.herokuapp.com/", :port => 587 }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
